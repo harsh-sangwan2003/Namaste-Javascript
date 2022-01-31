cc626909892d25bb498bce9fa5de567774fde7e9
harsh-sangwan2003 <hsangwan2002@gmail.com>
2022-01-31 19:49:01 +0530

    added

---
+++ 7_undefined/index.js
@@ -0,0 +1,16 @@
+console.log(a); //undefined
+var a = 10;
+console.log(x); //not defined
+
+if (a === undefined) {
+
+    console.log("a is undefined");
+}
+
+else {
+
+    console.log("Value of a is: ", a);
+}
+/* 
+undefined is special placeholder that is assigned to all the varaibles initially
+*/
---
+++ 7_undefined/index2.js
@@ -0,0 +1,8 @@
+var a;
+console.log(a);
+
+a = 10;
+console.log(a);
+
+a = "hello world";
+console.log(a);
